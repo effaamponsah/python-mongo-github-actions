4c27c080f90da3caebc2af7b615f27f511a6ad6d
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2020-09-30 09:12:06 +0000

    added simple functions

---
+++ maths.py
@@ -0,0 +1,10 @@
+def addition(num1, num2):
+    """
+    methods adds two parameters
+    """
+    return num1 + num2
+
+
+
+if __name__ == "__main__":
+    print(addition(12,2))
---
+++ test_maths.py
@@ -0,0 +1,6 @@
+import pytest
+from maths import addition
+
+
+def test_addition():
+   assert addition(1,2) == 3 
